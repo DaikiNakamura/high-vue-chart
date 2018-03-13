8979a09b673ffd34666ef3f24716111c9d672422
中村大騎 <d_nakamura@waku-2.com>
2018-03-13 12:11:10 +0900

    methods整理

--- src/HighVueChart.vue
+++ src/HighVueChart.vue
@@ -36,98 +36,147 @@ export default {
   	// hook change Series
     series: {
       handler: function(after) {
-        var before = this.oldSeries;
+        var before = this.beforeSeries;
         var vm = this;
-        after.forEach(function(series, index) {
+        after.forEach(function(afterSeries) {
           var sameBeforeSeries = before.filter(function(beforeSeries) {
-            return beforeSeries.name === series.name;
+            return beforeSeries.name === afterSeries.name;
           });
 
           // addNewSeries
           if(sameBeforeSeries.length === 0) {
-            vm.addSeries(JSON.parse(JSON.stringify(series)));
+            vm.addSeries(JSON.parse(JSON.stringify(afterSeries)));
             return;
           }
 
-          if(sameBeforeSeries[0].data.length === series.data.length) {
+          if(sameBeforeSeries[0].data.length === afterSeries.data.length) {
             // change val
-            for(var i = 0; i < series.data.length; i++) {
-              if(sameBeforeSeries[0].data[i] !== series.data[i]) {
-                vm.updateVal(i, series.name, series.data[i]);
+            for(var i = 0; i < afterSeries.data.length; i++) {
+              if(sameBeforeSeries[0].data[i] !== afterSeries.data[i]) {
+                vm.updateVal(i, afterSeries.name, afterSeries.data[i]);
               }
             }
           }
 
-          if(sameBeforeSeries[0].data.length !== series.data.length) {
+          if(sameBeforeSeries[0].data.length !== afterSeries.data.length) {
             // addNewPoint
-            var point = series.data[series.data.length - 1];
-            vm.addPoint(series.name, point);
+            var point = afterSeries.data[afterSeries.data.length - 1];
+            vm.addPoint(afterSeries.name, point);
             return;
           }
         });
-        this.oldSeries = JSON.parse(JSON.stringify(after));
+        this.beforeSeries = JSON.parse(JSON.stringify(after));
       },
       deep: true
     }
   },
   data: function() {
   	return {
-  	  chartObj: null,
-      nowSeries: null,
-      oldSeries: null
+  	  chart: null,
+      beforeSeries: null
   	};
   },
   mounted: function() {
 
-    this.nowSeries = JSON.parse(JSON.stringify(this.series));
+    var nowSeries = this.beforeSeries = JSON.parse(JSON.stringify(this.series));
+    var options = {
+      chart: {
+        type: this.type
+      },
+      title: {
+        text: this.title
+      },
+      series: nowSeries
+    };
 
-  	this.chartObj = Highcharts.chart('container', {
-        chart: {
-            type: this.type
-        },
-        title: {
-            text: this.title
-        },
-        series: this.nowSeries
-    });
-
-  	// copy
-  	this.oldSeries = JSON.parse(JSON.stringify(this.series));
+    var chart = this.createChart(options);
   },
   methods: {
-  	changeTitle: function(title) {
-  		this.chartObj.setTitle({ text: title });
-  	},
-    changeType: function(type) {
-  	  this.chartObj.update({
-        chart: {
-          type: type
-        }
-      })
-    },
-    addSeries: function(series) {
-  	  this.chartObj.addSeries(series);
-    },
-    addPoint: function(name, point) {
-  	  var targetSeries = this.chartObj.series.filter(function(series) {
-  	    return series.name === name;
-      });
 
-  	  if(targetSeries.length === 0) {
-  	    return;
+    // create Highcharts object
+    createChart: function(options) {
+
+  	  var chart = this.getChart();
+  	  if(chart) {
+  	    return chart;
       }
 
-      targetSeries[0].addPoint(point);
+      this.chart = Highcharts.chart('container', options);
+      return this.chart;
     },
-    updateVal: function(index, name, point) {
-      var targetSeries = this.chartObj.series.filter(function(series) {
+
+    getChart: function() {
+  	  return this.chart;
+    },
+
+    // get a Series from highcharts object.
+    getSeries: function(name) {
+
+  	  var chart = this.getChart();
+  	  if(!chart) {
+  	    return null;
+      }
+
+      var result = chart.series.filter(function(series) {
         return series.name === name;
       });
 
-      if(targetSeries.length === 0) {
+      if(result.length === 0) {
+        return null;
+      }
+
+      return result[0];
+    },
+
+    // --- call Highcharts method
+
+    changeTitle: function(title) {
+
+      var chart = this.getChart();
+      if(!chart) {
         return;
       }
-      targetSeries[0].data[index].update(parseInt(point, 10));
+
+      chart.setTitle({ text: title });
+    },
+
+    changeType: function(type) {
+
+      var chart = this.getChart();
+      if(!chart) {
+        return;
+      }
+
+      chart.update({
+        chart: {
+          type: type
+        }
+      });
+    },
+    addSeries: function(series) {
+
+      var chart = this.getChart();
+      if(!chart) {
+        return;
+      }
+
+      chart.addSeries(series);
+    },
+    addPoint: function(name, point) {
+      var series = this.getSeries(name);
+      if(!series) {
+        return;
+      }
+
+      series.addPoint(point);
+    },
+    updateVal: function(index, name, point) {
+      var series = this.getSeries(name);
+      if(!series) {
+        return;
+      }
+
+      series.data[index].update(parseInt(point, 10));
     }
   }
 }
